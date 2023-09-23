f6b37e816a52fd45f4a4cb9ba5e57c4326730ddb
shaade <s.shaade77@gmail.com>
2023-09-23 19:57:36 +0200

    fix subtitle size

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -13,7 +13,9 @@ export function AppLayout({ children }: TAppLayoutProps) {
         <Title c="gray.8" order={2}>
           My Tasks
         </Title>
-        <Text c="dimmed">Saturday 23th of September 2023</Text>
+        <Text c="dimmed" size="sm">
+          Saturday 23th of September 2023
+        </Text>
       </Stack>
 
       <Space h="xl" />
