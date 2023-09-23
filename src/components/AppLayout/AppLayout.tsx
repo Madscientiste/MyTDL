e060f80ef825337b1ee81fa887c5307f3b8874e0
shaade <s.shaade77@gmail.com>
2023-09-23 19:57:04 +0200

    add header to AppLayout

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-import { Container, Stack } from "@mantine/core";
+import { Container, Space, Stack, Text, Title } from "@mantine/core";
 
 type TAppLayoutProps = {
   children: ReactNode;
@@ -9,6 +9,15 @@ type TAppLayoutProps = {
 export function AppLayout({ children }: TAppLayoutProps) {
   return (
     <Container size="md" pt="xl">
+      <Stack gap="0.25em">
+        <Title c="gray.8" order={2}>
+          My Tasks
+        </Title>
+        <Text c="dimmed">Saturday 23th of September 2023</Text>
+      </Stack>
+
+      <Space h="xl" />
+
       <Stack>{children}</Stack>
     </Container>
   );
