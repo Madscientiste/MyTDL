521e36a8458f757a4741b0803b1594ef9f3e218c
shaade <s.shaade77@gmail.com>
2023-09-26 10:42:23 +0200

    chore: cleanup

--- src/components/ErrorBoundary/GlobalErrorBoundary.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-
-import { Fallback } from "./Fallback";
-
-export function GlobalErrorBoundary({ children }: { children: ReactNode }) {
-  return <ErrorBoundary FallbackComponent={Fallback}>{children}</ErrorBoundary>;
-}
