cf1a67e6d962a797e4fdbb9a252cceb014000aca
shaade <s.shaade77@gmail.com>
2023-09-26 09:50:15 +0200

    drop json api, and use python backend instead

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,5 @@
 import Axios from "axios";
 
-const JSON_API = "https://jsonplaceholder.typicode.com";
-export const axios = Axios.create({ baseURL: JSON_API, timeout: 20000 });
+export const axios = Axios.create({ baseURL: "/api", timeout: 20000 });
 
 axios.interceptors.response.use((response) => response.data);
