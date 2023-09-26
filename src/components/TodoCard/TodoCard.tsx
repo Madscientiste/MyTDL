dccf58113756d7b48cd9ebd298a0f89a72fdcae5
shaade <s.shaade77@gmail.com>
2023-09-26 09:49:51 +0200

    update todocard's style

--- src/components/TodoCard/TodoCard.tsx
+++ src/components/TodoCard/TodoCard.tsx
@@ -1,27 +1,30 @@
-import { useCallback, useState } from "react";
+import { memo, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { ActionIcon, Box, Checkbox, Divider, Group, Paper, Stack, Text } from "@mantine/core";
 import { IconChevronRight } from "@tabler/icons-react";
 
+import { useUpdateTodo } from "@/api/updateTodo";
 import { TTodoItem } from "@/type";
 
 import classes from "./TodoCard.module.css";
 
-type TTodoCardProps = {
-  todoItem: Pick<TTodoItem, "id" | "title" | "content" | "createdAt" | "completedAt">;
-};
+type TTodoCardProps = Pick<TTodoItem, "id" | "title" | "completed">;
 
-export function TodoCard({ todoItem }: TTodoCardProps) {
-  const [checked, setChecked] = useState(!!todoItem.completedAt);
+function _TodoCard({ id, title, completed: checked }: TTodoCardProps) {
+  const mutation = useUpdateTodo();
   const navigate = useNavigate();
 
   const handleClick = useCallback(() => {
-    setChecked((v) => !v);
-  }, []);
+    mutation.mutate({ id, data: { completed: !checked } });
+  }, [checked]);
+
+  const handleNavigate = useCallback(() => {
+    navigate(`/todos/${id}`);
+  }, [id]);
 
   return (
-    <Paper className={classes.paper} withBorder>
+    <Paper data-checked={`${checked}`} className={classes.paper} withBorder>
       <Group gap={0} align="stretch" wrap="nowrap">
         <Stack className={classes.leftSection} onClick={handleClick}>
           <Box className={classes.checkboxWrapper} onClick={handleClick}>
@@ -34,13 +37,13 @@ export function TodoCard({ todoItem }: TTodoCardProps) {
           </Box>
         </Stack>
 
-        <Divider color="gray.2" size="sm" orientation="vertical" />
+        <Divider size="sm" orientation="vertical" />
 
         {/* I think its better if we can click on the whole thing to see its details, rather than clicking on the leetle tinie chevron..
           But keeping the checkbox's behaviour to mark an item as completed. */}
-        <Group className={classes.sectionWrapper} onClick={() => navigate(`/todos/${todoItem.id}`)}>
+        <Group className={classes.sectionWrapper} onClick={handleNavigate}>
           <Stack className={classes.middleSection}>
-            <Text td={checked ? "line-through" : undefined}>{todoItem.title}</Text>
+            <Text>{title}</Text>
           </Stack>
 
           <Stack className={classes.rightSection}>
@@ -53,3 +56,6 @@ export function TodoCard({ todoItem }: TTodoCardProps) {
     </Paper>
   );
 }
+
+// Realisticly, we are only updating the checked status when clicking, so ..
+export const TodoCard = memo(_TodoCard);
