004d8d50d4e5740b0fb0e01660f3c7c71934f6ce
shaade <s.shaade77@gmail.com>
2023-09-27 11:47:18 +0200

    feat: user can add a task

--- src/api/createTodo.ts
+++ src/api/createTodo.ts
@@ -1,6 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { UseMutationOptions, useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { produce } from "immer";
 
 import { axios } from "@/lib/axios";
+import { notifications } from "@/lib/notifications";
+import { queryClient } from "@/lib/react-query";
 import { TTodoItem } from "@/type";
 
 export type TCreateTodoPayload = {
@@ -14,6 +18,43 @@ export const createTodo = ({ data }: TCreateTodoPayload): Promise<TTodoItem> => 
   return axios.post("/todos/", data);
 };
 
-export const useCreateTodo = () => {
-  return useMutation({});
+type TUseCreateTodoOptions = {
+  config?: UseMutationOptions<TTodoItem, AxiosError, TCreateTodoPayload>;
+};
+
+export const useCreateTodo = ({ config }: TUseCreateTodoOptions = {}) => {
+  return useMutation({
+    ...config,
+
+    mutationFn: createTodo,
+    onMutate: async (variables) => {
+      await queryClient.cancelQueries(["todos"]);
+
+      const prevTodos = queryClient.getQueryData<TTodoItem[]>(["todos"]) || [];
+
+      queryClient.setQueryData<TTodoItem[]>(
+        ["todos"],
+        produce(prevTodos, (draft) => {
+          const items = prevTodos.filter((t) => !t.completed);
+          const last = items[items.length - 1];
+          const idx = draft.indexOf(last);
+
+          return [
+            //
+            ...draft.slice(0, idx),
+            variables.data as TTodoItem,
+            ...draft.slice(idx),
+          ];
+        }),
+      );
+
+      return { prevTodos };
+    },
+    onError: async () => {
+      notifications.error({ message: "Something went wrong updating your item, try again !" });
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["todos"]);
+    },
+  });
 };
--- src/components/CreateTodo/CreateTodo.tsx
+++ src/components/CreateTodo/CreateTodo.tsx
@@ -1,18 +1,41 @@
+import { FormEvent } from "react";
+
 import { Button, TextInput } from "@mantine/core";
+
+import { useCreateTodo } from "@/api/createTodo";
 
 import classes from "./CreateTodo.module.css";
 
+type TFormElements = HTMLFormElement & {
+  readonly elements: HTMLFormControlsCollection & {
+    title: HTMLInputElement;
+  };
+};
+
 export function CreateTodo() {
-  // const todo = useCreateTodo();
+  const mutation = useCreateTodo();
+
+  const handleSubmit = async (e: FormEvent<TFormElements>) => {
+    e.preventDefault();
+    const elements = e.currentTarget.elements;
+    const title = elements.title.value;
+
+    await mutation.mutateAsync({ data: { title } });
+  };
 
   return (
-    <div className={classes.controls}>
-      <TextInput
-        placeholder="Bake cookies using firefox"
-        classNames={{ input: classes.input, root: classes.inputWrapper }}
-      />
+    <form onSubmit={handleSubmit}>
+      <div className={classes.controls}>
+        <TextInput
+          name="title"
+          placeholder="Bake cookies using firefox"
+          classNames={{ input: classes.input, root: classes.inputWrapper }}
+        />
 
-      <Button className={classes.control} >Add</Button>
-    </div>
+        <Button type="submit" className={classes.control} loading={mutation.isLoading}>
+          Add
+        </Button>
+      </div>
+    </form>
   );
 }
