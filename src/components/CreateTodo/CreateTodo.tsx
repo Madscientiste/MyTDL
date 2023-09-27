7311b378721c7913afe7992aa27d3d1d85ca5f37
shaade <s.shaade77@gmail.com>
2023-09-27 18:46:31 +0200

    add button to show additional input

--- src/components/CreateTodo/CreateTodo.tsx
+++ src/components/CreateTodo/CreateTodo.tsx
@@ -1,6 +1,7 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
-import { Button, TextInput } from "@mantine/core";
+import { Button, Stack, TextInput } from "@mantine/core";
+import { IconPlus } from "@tabler/icons-react";
 
 import { useCreateTodo } from "@/api/createTodo";
 
@@ -9,28 +10,50 @@ import classes from "./CreateTodo.module.css";
 type TFormElements = HTMLFormElement & {
   readonly elements: HTMLFormControlsCollection & {
     title: HTMLInputElement;
+    description: HTMLInputElement;
   };
 };
 
 export function CreateTodo() {
   const mutation = useCreateTodo();
+  const [showDesc, setDesc] = useState(false);
 
   const handleSubmit = async (e: FormEvent<TFormElements>) => {
     e.preventDefault();
+
     const elements = e.currentTarget.elements;
     const title = elements.title.value;
+    const description = elements.description.value;
 
-    await mutation.mutateAsync({ data: { title } });
+    const vars = { data: { title, description } };
+    await mutation.mutateAsync(vars, {
+      onSuccess: () => {
+        elements.title.value = "";
+        elements.description.value = "";
+      },
+    });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className={classes.controls}>
-        <TextInput
-          name="title"
-          placeholder="Bake cookies using firefox"
-          classNames={{ input: classes.input, root: classes.inputWrapper }}
-        />
+        <Stack style={{ width: "100%" }} gap="xs">
+          <TextInput
+            name="title"
+            placeholder="Title"
+            classNames={{ input: classes.input, root: classes.inputWrapper }}
+          />
+
+          {!showDesc && (
+            <Button className={classes.button} leftSection={<IconPlus size={16} />} onClick={() => setDesc(true)}>
+              Add description
+            </Button>
+          )}
+
+          {showDesc && (
+            <TextInput name="description" placeholder="Description" classNames={{ root: classes.inputWrapper }} />
+          )}
+        </Stack>
 
         <Button type="submit" className={classes.control} loading={mutation.isLoading}>
           Add
