d09fa9e1cf138e7b243c9243e2dab513094453b1
shaade <s.shaade77@gmail.com>
2023-09-28 18:18:07 +0200

    add function initMock before initial app render

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@
 // These are the two main chunks that are used to render the core structure of the app
 // Importing them with Promise.all (by using HTTP/2 multiplexing) we can load them in parallel
 // and achieve the best possible performance
+import { initMocks } from "@test/server";
+
+initMocks();
 
 Promise.all([import("@/Root"), import("@/App")]).then(([{ default: render }, { default: App }]) => {
   render(App);
