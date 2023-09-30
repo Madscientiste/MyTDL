3dfb7a26f14e3c6a0789e4dfb2fcb410f33d9e2a
shaade <s.shaade77@gmail.com>
2023-09-30 09:10:25 +0200

    fix ErrorBoundary crashing when encoutering a simple error

--- src/components/GlobalErrorBoundary/Fallback.tsx
+++ src/components/GlobalErrorBoundary/Fallback.tsx
@@ -1,14 +1,20 @@
-import { FallbackProps } from "react-error-boundary";
-
 import { Alert, Button, Group, Modal, Paper, Stack, Text, ThemeIcon } from "@mantine/core";
 import { IconAlertTriangle, IconBug } from "@tabler/icons-react";
 import { AxiosError } from "axios";
 
-export function Fallback({ error, resetErrorBoundary }: FallbackProps) {
-  const shouldOpen = error instanceof AxiosError;
+type TFallbackProps = {
+  error: AxiosError | Error;
+  resetErrorBoundary: (...args: any[]) => void;
+};
+
+export function Fallback({ error, resetErrorBoundary }: TFallbackProps) {
+  const isAxiosError = error instanceof AxiosError;
+
+  const title = isAxiosError ? error.response?.statusText : error.name;
+  const body = error.message;
 
   return (
-    <Modal.Root onClose={resetErrorBoundary} opened={shouldOpen} centered>
+    <Modal.Root onClose={resetErrorBoundary} opened centered>
       <Modal.Overlay />
 
       <Modal.Content>
@@ -20,13 +26,13 @@ export function Fallback({ error, resetErrorBoundary }: FallbackProps) {
               </ThemeIcon>
             </Group>
 
-            <Alert variant="filled" color="red" title={error.response.statusText} icon={<IconBug />}>
+            <Alert variant="filled" color="red" title={title} icon={<IconBug />}>
               Unable to reach the server, make sure everything is configured proprely !
             </Alert>
 
             <Paper p="md" withBorder>
               <Text c="gray.7" size="sm">
-                {error.message}
+                {body}
               </Text>
             </Paper>
 
