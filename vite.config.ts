a3e7491d4ed58c24028d07a5529d28df2064dc7c
shaade <s.shaade77@gmail.com>
2023-10-02 20:56:54 +0200

    use vite modes to enable or disable fake backend

--- vite.config.ts
+++ vite.config.ts
@@ -5,51 +5,53 @@ import { defineConfig } from "vite";
 const usePath = (_path: string, ...other: string[]) => path.join(__dirname, _path, ...other);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
 
-  define: {
-    __VITE_ENABLE_FAKE_BACKEND__: true,
-  },
+    define: {
+      __VITE_ENABLE_FAKE_BACKEND__: mode === "github",
+    },
 
-  test: {
-    globals: true,
-    environment: "jsdom",
-    include: ["src/**/*.test.{ts,tsx}"],
-    setupFiles: [path.join(usePath("./test/setup.ts"))],
-  },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      include: ["src/**/*.test.{ts,tsx}"],
+      setupFiles: [path.join(usePath("./test/setup.ts"))],
+    },
 
-  resolve: {
-    alias: [
-      { find: "@", replacement: path.join(usePath("./src")) },
-      { find: "@test", replacement: path.join(usePath("./test")) },
-    ],
-  },
+    resolve: {
+      alias: [
+        { find: "@", replacement: path.join(usePath("./src")) },
+        { find: "@test", replacement: path.join(usePath("./test")) },
+      ],
+    },
 
-  build: {
-    outDir: "dist",
-    manifest: true,
-    emptyOutDir: false,
-    rollupOptions: {
-      input: path.join(usePath("./src"), "index.tsx"),
-      output: {
-        chunkFileNames: "js/[name].chunk-[hash].js",
-        entryFileNames: "js/[name]-[hash].js",
-        assetFileNames: (asset: any) => {
-          const isFont = /\.(woff|woff2|eot|ttf|otf)$/.test(asset.name);
-          const group = isFont ? "fonts" : "assets";
-          return `${group}/[name].[ext]`;
+    build: {
+      outDir: "dist",
+      manifest: true,
+      emptyOutDir: false,
+      rollupOptions: {
+        input: path.join(usePath("./src"), "index.tsx"),
+        output: {
+          chunkFileNames: "js/[name].chunk-[hash].js",
+          entryFileNames: "js/[name]-[hash].js",
+          assetFileNames: (asset: any) => {
+            const isFont = /\.(woff|woff2|eot|ttf|otf)$/.test(asset.name);
+            const group = isFont ? "fonts" : "assets";
+            return `${group}/[name].[ext]`;
+          },
         },
       },
     },
-  },
 
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        secure: false,
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:8000",
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
